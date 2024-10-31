99879f072298a775253fe5c3572139ec3d678c14
mhanik00 <mhanik063@gmail.com>
2024-10-31 17:11:34 +0600

    n

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -208,7 +208,8 @@ function HomePage() {
         </h3>
         <div className="flex flex-col md:flex-row justify-center space-y-4 md:space-y-0 md:space-x-4">
           {/* Testimonial from Sarah L. */}
-          <div className="bg-white shadow-md rounded-lg p-4 max-w-xs flex items-start">
+          <div className="bg-white shadow-md rounded-lg p-4
+           max-w-xs flex  items-center">
             <img
               src={user1}
               alt="Profile of Sarah L."
